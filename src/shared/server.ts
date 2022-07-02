ed7c39b13f8ca1ad682651a137ff7776963b1ae3
vinicius resende <vinicius.resende@skorr.social>
2022-07-02 09:35:04 -0300

    kahbot initial stg

--- src/shared/server.ts
+++ src/shared/server.ts
@@ -4,35 +4,7 @@ import ChannelBoardService from "../service/ChannelService";
 import { Emotes } from "../utils/emotes";
 import axiosClient from "./connection/client";
 
-//client=fjm3cu1ed4xsfbu1wl5cfkswy7np8r
-//secret=yt2ux8fy2o8bsrq0ci9ad123fl9bz9
-
-/*
-{
-	"access_token": "tw8r76tfsggfhut7s2v8bh3aynp05z",
-	"expires_in": 5661274,
-	"token_type": "bearer"
-}
-*/
-
-/*
-{
-	"data": [
-		{
-			"id": "141981764",
-			"login": "twitchdev",
-			"display_name": "TwitchDev",
-			"type": "",
-			"broadcaster_type": "partner",
-			"description": "Supporting third-party developers building Twitch integrations from chatbots to game integrations.",
-			"profile_image_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/8a6381c7-d0c0-4576-b179-38bd5ce1d6af-profile_image-300x300.png",
-			"offline_image_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/3f13ab61-ec78-4fe6-8481-8682cb3b0ac2-channel_offline_image-1920x1080.png",
-			"view_count": 19044088,
-			"created_at": "2016-12-14T20:32:28Z"
-		}
-	]
-}
-*/
+const port = process.env.PORT || 8080;
 
 const client = new tmi.Client({
   options: { debug: true },
